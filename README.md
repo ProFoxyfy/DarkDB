2502aedf0c32e8e49d19a455687a8e68a574ae44
DarkAndWhite <79207126+ProFoxyfy@users.noreply.github.com>
2023-03-09 16:25:30 +0100

    Add main package and README.md

---
+++ README.md
@@ -0,0 +1,35 @@
+# Why?
+
+Because why not! I originally created this project for future things.
+
+# How?
+
+Well, Python of course. Even though its slow, its enough for a custom client-side database engine.
+
+# How do i use it?
+
+Its simple.
+You also need to put the DarkDB source files in your source directory, 
+
+    import darkdb
+    darkdb.makeDatabase("mydatabase.db")
+    # add some example
+    darkdb.addRow("MyRowName","MyRowValue","mydatabase.db")
+    # remove that example
+    darkdb.removeRow("MyRowName","mydatabase.db")
+    # read all lines as string
+    print(darkdb.dbAsString("mydatabase.db"))
+If you want to delete a database simply just delete the database file.
+Its not hard.
+
+# Pros and Cons
+
+## Pros
+ - Easy to use
+ - Automatically saves beetween actions
+ - Can save to a file
+ 
+## Cons
+ - Does'nt support server-side without modifications.
+ - No snapshot beetween saves
+ - Only supports Python
---
+++ src/reader.py
@@ -0,0 +1,42 @@
+# format:
+# name:value;
+# anotherName:anotherValue;
+#
+# this means that for the name or value, ":" and ";" are invalid characters.
+
+class Row:
+    Name = ""
+    Value = None
+
+
+def readDbAsTable(filename):
+    result = []
+    file = open(filename, "r")
+    lines = file.read().split(";")
+
+    for stri in lines:
+        _newrow = Row()
+        _splitstr = stri.split(":")
+
+        if _splitstr.__len__() > 1:  # if the length is lower than 2 we think it's a newline
+            _newrow.Name = _splitstr[0]
+            _newrow.Value = _splitstr[1]
+
+        result.append(_newrow)
+
+    return result
+
+
+def saveTable(table, filename, overwrite):
+    content = ""
+
+    for row in table:
+        content += row.Name + ":" + row.Value + ";"
+        content += "\n"
+
+    currentWriteMode = "a"
+    if overwrite:
+        currentWriteMode = "w"
+
+    newFile = open(filename, currentWriteMode)
+    newFile.write(content)
