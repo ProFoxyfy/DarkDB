85f2d9c7e65e1110fcd214f7e36dac7c5a74d921
DarkAndWhite <79207126+ProFoxyfy@users.noreply.github.com>
2023-07-23 20:31:11 +0200

    Update tester.py

--- src/tester.py
+++ src/tester.py
@@ -4,31 +4,31 @@ try:
   darkdb.makeDatabase("unittest")
   print("Test 1: Success")
 except:
-  error("Test 1: Fail")
-  assert false
+  print("Test 1: Fail")
+  assert False
 
 try:
   darkdb.addRow("newrow","test","unittest")
   print("Test 2: Success")
 except:
-  error("Test 2: Fail")
+  print("Test 2: Fail")
   assert false
 
 try:
   darkdb.changeRow("newrow","NiceOne","unittest", True)
   print("Test 2: Success")
 except:
-  error("Test 2: Fail")
+  print("Test 2: Fail")
   assert false
 
 try:
   assert darkdb.dbAsString("unittest")
   print("Test 3: Success")
 except:
-  error("Test 3: Fail")
+  print("Test 3: Fail")
 
 try:
   assert darkdb.dbAsTableOfRows("unittest")
   print("Test 4: Success")
 except:
-  error("Test 4: Fail")
+  print("Test 4: Fail")
