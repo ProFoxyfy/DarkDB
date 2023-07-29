5c2cdd38a18eebabf32162e87e2fd6ed68c16278
DarkAndWhite <79207126+ProFoxyfy@users.noreply.github.com>
2023-07-29 15:02:12 +0200

    patch because apparently i cant count

--- src/tester.py
+++ src/tester.py
@@ -16,19 +16,19 @@ except:
 
 try:
   darkdb.changeRow("newrow","NiceOne","unittest", True)
-  print("Test 2: Success")
+  print("Test 3: Success")
 except:
-  print("Test 2: Fail")
+  print("Test 3: Fail")
   assert False
 
 try:
   assert darkdb.dbAsString("unittest")
-  print("Test 3: Success")
-except:
-  print("Test 3: Fail")
-
-try:
-  assert darkdb.dbAsTableOfRows("unittest")
   print("Test 4: Success")
 except:
   print("Test 4: Fail")
+
+try:
+  assert darkdb.dbAsTableOfRows("unittest")
+  print("Test 5: Success")
+except:
+  print("Test 5: Fail")
