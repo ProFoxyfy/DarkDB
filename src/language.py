e6f131411d65625639b1f213beb0f1b8cac0d93e
DarkAndWhite <79207126+ProFoxyfy@users.noreply.github.com>
2023-09-26 15:31:40 +0200

    Update the languages...

--- src/language.py
+++ src/language.py
@@ -2,7 +2,7 @@ lang_strings = {
     "german": {
         "download_start": "====== HERUNTERLADEN VON '%' ======",
         "download_end": "====== HERUNTERLADEN FERTIG ======",
-        "install_end": "INSTALLIEREN VON DARKDB IST FERTIG; BITTE NUTZEN SIE 'import darkdb.darkdb' IM PROJEKT",
+        "install_end": "INSTALLIEREN VON DARKDB IST FERTIG; BITTE NUTZEN SIE 'import darkdb' IM PROJEKT",
         "select_branch": "BITTE VERSION AUSWÄHLEN\n[1] production\n[2] experimental",
         "folder_create": "ERSTELLEN VOM INSTALATIONSORDNER...",
         "select_project_folder": "Wählen sie ihren Projektordner aus"
@@ -10,7 +10,7 @@ lang_strings = {
     "english": {
         "download_start": "====== DOWNLOAD FROM '%' ======",
         "download_end": "====== DOWNLOAD COMPLETED ======",
-        "install_end": "DARKDB INSTALLATION IS COMPLETED; PLEASE USE 'import darkdb.darldb' IN YOUR PROJECT",
+        "install_end": "DARKDB INSTALLATION IS COMPLETED; PLEASE USE 'import darkdb' IN YOUR PROJECT",
         "select_branch": "PLEASE SELECT BRANCH\n[1] production\n[2] experimental",
         "folder_create": "CREATING INSTALLATION FOLDER...",
         "select_project_folder": "Please select the folder of your project"
